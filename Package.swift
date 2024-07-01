41a46c8cb3539f20592f991d9dbf5e2c5846ae3d
GangWoon <gangwoon2018@gmail.com>
2024-07-01 19:59:50 +0900

    Add TabBar module

--- Package.swift
+++ Package.swift
@@ -6,9 +6,18 @@ let package = Package(
   platforms: [.iOS(.v17)],
   products: [
     .library(name: "Concurrency", targets: ["Concurrency"]),
+    .library(
+      name: "Views",
+      targets: [
+        "ScrollView",
+        "TabBar"
+      ]
+    )
   ],
   targets: [
-    .target(name: "Concurrency")
+    .target(name: "Concurrency"),
+    .target(name: "ScrollView"),
+    .target(name: "TabBar")
   ],
   swiftLanguageVersions: [.v6]
 )
